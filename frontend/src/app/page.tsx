a64fb7b003fb832f52393c595b883aa755964bf6
ticlekiwi <florian.nicolas@yahoo.fr>
2025-03-01 13:33:42 +0100

    updating frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,31 @@
 import { StoreFinderConfig } from "@/lib/definitions";
 import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 
 const StoreFinder = dynamic(() => import("../components/store-finder"));
 
-export default function Home({ config = {} }: { config: StoreFinderConfig }) {
-  return <StoreFinder config={{
-    ...config,
-    apiKey: '4d5b7598-7ce4-4533-b0c0-f2147b175fd9',
-    label: 'Vapiano',
-    style: {
-      mainColor: '#D40128',
+const VAPIANO_CONFIG = {
+  apiKey: '4d5b7598-7ce4-4533-b0c0-f2147b175fd9',
+  label: 'Vapiano',
+  style: {
+    mainColor: '#D40128',
+  },
+};
+
+export default function Home() {
+  const [config, setConfig] = useState<StoreFinderConfig | null>(VAPIANO_CONFIG);
+
+  useEffect(() => {
+    const windowWithConfig = window as unknown as Window & { 
+      storeLocatoreConfig: StoreFinderConfig 
+    };
+    
+    if ('storeLocatoreConfig' in windowWithConfig) {
+      setConfig(windowWithConfig.storeLocatoreConfig);
     }
-  }} />;
+  }, []);
+
+  if (!config) return null;
+
+  return <StoreFinder config={config} />;
 }
