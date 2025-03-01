f319e00bcf640f485f159cf13e2866d2e761617d
ticlekiwi <florian.nicolas@yahoo.fr>
2025-03-01 14:54:02 +0100

    updating frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Génère un site statique (SSG)
+  distDir: "dist",   // Dossier de sortie pour les fichiers statiques
+  assetPrefix: "./",  // Pour gérer correctement les chemins relatifs
+  trailingSlash: true // Pour ajouter un "/" à la fin des URLs et éviter les erreurs 404
 };
 
 export default nextConfig;
