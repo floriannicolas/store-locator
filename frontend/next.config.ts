af82c80ef5a54299994f4d0562175909d5508e44
ticlekiwi <florian.nicolas@yahoo.fr>
2025-03-01 14:56:53 +0100

    updating frontend assetPrefix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export", // Génère un site statique (SSG)
   distDir: "dist",   // Dossier de sortie pour les fichiers statiques
-  assetPrefix: "./",  // Pour gérer correctement les chemins relatifs
+  assetPrefix: "/",  // Pour gérer correctement les chemins relatifs
   trailingSlash: true // Pour ajouter un "/" à la fin des URLs et éviter les erreurs 404
 };
 
